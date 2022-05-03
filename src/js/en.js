37eee3bba50f2053feecabd8feeab935b2cb2cae
Mihail Samofalov <rhbkickshot@gmail.com>
2022-05-03 16:44:34 +0300

    feat: add project structure

---
+++ src/js/en.js
@@ -0,0 +1,320 @@
+const en = [
+    {
+        small: '`',
+        shift: '~',
+        code: 'Backquote',
+    },
+    {
+        small: '1',
+        shift: '!',
+        code: 'Digit1',
+    },
+    {
+        small: '2',
+        shift: '@',
+        code: 'Digit2',
+    },
+    {
+        small: '3',
+        shift: '#',
+        code: 'Digit3',
+    },
+    {
+        small: '4',
+        shift: '$',
+        code: 'Digit4',
+    },
+    {
+        small: '5',
+        shift: '%',
+        code: 'Digit5',
+    },
+    {
+        small: '6',
+        shift: '^',
+        code: 'Digit6',
+    },
+    {
+        small: '7',
+        shift: '&',
+        code: 'Digit7',
+    },
+    {
+        small: '8',
+        shift: '*',
+        code: 'Digit8',
+    },
+    {
+        small: '9',
+        shift: '(',
+        code: 'Digit9',
+    },
+    {
+        small: '0',
+        shift: ')',
+        code: 'Digit0',
+    },
+    {
+        small: '-',
+        shift: '_',
+        code: 'Minus',
+    },
+    {
+        small: '=',
+        shift: '+',
+        code: 'Equal',
+    },
+    {
+        small: 'Backspace',
+        shift: null,
+        code: 'Backspace',
+    },
+    {
+        small: 'Tab',
+        shift: null,
+        code: 'Tab',
+    },
+    {
+        small: 'q',
+        shift: 'Q',
+        code: 'KeyQ',
+    },
+    {
+        small: 'w',
+        shift: 'W',
+        code: 'KeyW',
+    },
+    {
+        small: 'e',
+        shift: 'E',
+        code: 'KeyE',
+    },
+    {
+        small: 'r',
+        shift: 'R',
+        code: 'KeyR',
+    },
+    {
+        small: 't',
+        shift: 'T',
+        code: 'KeyT',
+    },
+    {
+        small: 'y',
+        shift: 'Y',
+        code: 'KeyY',
+    },
+    {
+        small: 'u',
+        shift: 'U',
+        code: 'KeyU',
+    },
+    {
+        small: 'i',
+        shift: 'I',
+        code: 'KeyI',
+    },
+    {
+        small: 'o',
+        shift: 'O',
+        code: 'KeyO',
+    },
+    {
+        small: 'p',
+        shift: 'P',
+        code: 'KeyP',
+    },
+    {
+        small: '[',
+        shift: '{',
+        code: 'BracketLeft',
+    },
+    {
+        small: ']',
+        shift: '}',
+        code: 'BracketRight',
+    },
+    {
+        small: '',
+        shift: '|',
+        code: 'BackSlash',
+    },
+    {
+        small: 'CapsLock',
+        shift: null,
+        code: 'CapsLock',
+    },
+    {
+        small: 'a',
+        shift: 'A',
+        code: 'KeyA',
+    },
+    {
+        small: 's',
+        shift: 'S',
+        code: 'KeyS',
+    },
+    {
+        small: 'd',
+        shift: 'D',
+        code: 'KeyD',
+    },
+    {
+        small: 'f',
+        shift: 'F',
+        code: 'KeyF',
+    },
+    {
+        small: 'g',
+        shift: 'G',
+        code: 'KeyG',
+    },
+    {
+        small: 'h',
+        shift: 'H',
+        code: 'KeyH',
+    },
+    {
+        small: 'j',
+        shift: 'J',
+        code: 'KeyJ',
+    },
+    {
+        small: 'k',
+        shift: 'K',
+        code: 'KeyK',
+    },
+    {
+        small: 'l',
+        shift: 'L',
+        code: 'KeyL',
+    },
+    {
+        small: ';',
+        shift: ':',
+        code: 'Semicolon',
+    },
+    {
+        small: "'",
+        shift: '"',
+        code: 'Quote',
+    },
+    {
+        small: 'Enter',
+        shift: null,
+        code: 'Enter',
+    },
+    {
+        small: 'Shift',
+        shift: null,
+        code: 'ShiftLeft',
+    },
+    {
+        small: 'z',
+        shift: 'Z',
+        code: 'KeyZ',
+    },
+    {
+        small: 'x',
+        shift: 'X',
+        code: 'KeyX',
+    },
+    {
+        small: 'c',
+        shift: 'C',
+        code: 'KeyC',
+    },
+    {
+        small: 'v',
+        shift: 'V',
+        code: 'KeyV',
+    },
+    {
+        small: 'b',
+        shift: 'B',
+        code: 'KeyB',
+    },
+    {
+        small: 'n',
+        shift: 'N',
+        code: 'KeyN',
+    },
+    {
+        small: 'm',
+        shift: 'M',
+        code: 'KeyM',
+    },
+    {
+        small: ',',
+        shift: '<',
+        code: 'Comma',
+    },
+    {
+        small: '.',
+        shift: '>',
+        code: 'Period',
+    },
+    {
+        small: '/',
+        shift: '?',
+        code: 'Slash',
+    },
+    {
+        small: '',
+        shift: '',
+        code: 'ArrowUp',
+    },
+    {
+        small: 'Shift',
+        shift: null,
+        code: 'ShiftRight',
+    },
+    {
+        small: 'Ctrl',
+        shift: null,
+        code: 'ControlLeft',
+    },
+    {
+        small: 'Meta',
+        shift: null,
+        code: 'MetaLeft',
+    },
+    {
+        small: 'Alt',
+        shift: null,
+        code: 'AltLeft',
+    },
+    {
+        small: '',
+        shift: '',
+        code: 'Space',
+    },
+    {
+        small: 'Alt',
+        shift: null,
+        code: 'AltRight',
+    },
+    {
+        small: 'Ctrl',
+        shift: null,
+        code: 'ControlRight',
+    },
+    {
+        small: '',
+        shift: '',
+        code: 'ArrowLeft',
+    },
+    {
+        small: '',
+        shift: '',
+        code: 'ArrowDown',
+    },
+    {
+        small: '',
+        shift: '',
+        code: 'ArrowRight',
+    }
+]
+
+
+export default en;
---
+++ src/js/rowOrder.js
@@ -0,0 +1,9 @@
+const rowOrder = [
+    ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
+    ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'BackSlash'],
+    ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'],
+    ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight'],
+    ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight']
+]
+
+export default rowOrder;
